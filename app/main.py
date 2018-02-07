a30cf195e29d25fd3ffedbc37f2eba5aadeac56c
Tyrel Hiebert <tyrelh@uvic.ca>
2018-02-07 14:55:30 -0800

    Change snake color and name

--- app/main.py
+++ app/main.py
@@ -23,10 +23,10 @@ def start():
     # TODO: Do things with data
 
     return {
-        'color': '#00FF00',
+        'color': '#99ccff',
         'taunt': '{} ({}x{})'.format(game_id, board_width, board_height),
         'head_url': head_url,
-        'name': 'battlesnake-python'
+        'name': 'BaffleSnek'
     }
 
 
