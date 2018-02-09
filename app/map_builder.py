7996851002847c9fcc9cfa2b70d408ea017630c1
Tyrel Hiebert <tyrelh@uvic.ca>
2018-02-09 15:23:29 -0800

    Swap height and width

--- app/map_builder.py
+++ app/map_builder.py
@@ -7,7 +7,7 @@ SNAKE_HEAD = 4
 
 def build_map(data):
     # create map and fill with zeros
-    map = [ [0 for col in range(data['height'])] for row in range(data['width'])]
+    map = [ [0 for col in range(data['width'])] for row in range(data['height'])]
     # fill in food locations
     for food in data['food']['data']:
         map[food['x']][food['y']] = FOOD
