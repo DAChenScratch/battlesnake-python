974bcc24958c08acd553b65b07ae2813932f8dff
Tyrel Hiebert <tyrelh@uvic.ca>
2018-02-09 15:04:57 -0800

    Fix map building

--- app/map_builder.py
+++ app/map_builder.py
@@ -6,16 +6,16 @@ SNAKE_HEAD = 4
 
 
 def build_map(data):
+    # create map and fill with zeros
     map = [ [0 for col in range(data['height'])] for row in range(data['width'])]
-
+    # fill in food locations
     for food in data['food']['data']:
+        food = {food['x'], food['y']}
         map[food[0]][food[1]] = FOOD
-
     return map
 
 
 def print_map(map):
-
     for i in map:
         for j in i:
             print(str(map[i][j]) + ' ')
