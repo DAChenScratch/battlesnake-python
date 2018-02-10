7a5da7f817b37198586795c0e924bb805219389b
Tyrel Hiebert <tyrelh@uvic.ca>
2018-02-10 09:06:16 -0800

    Switched map print to compare runtime

--- app/main.py
+++ app/main.py
@@ -49,8 +49,8 @@ def move():
     directions = ['up', 'left', 'down', 'right']
     test_direction = test_ai(test_direction, data)
 
-    #print(directions[test_direction])
-    print_map(map, data['width'], data['height'])
+    print(directions[test_direction])
+    #print_map(map, data['width'], data['height'])
     display_map(map)
     end_time = time.time()
     print('Time for move was ' + str((end_time - start_time) * 1000) + 'ms')
