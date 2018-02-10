d36d09337a26ffe6705b12ba06cb28f98c42c155
Tyrel Hiebert <tyrelh@uvic.ca>
2018-02-10 08:51:22 -0800

    Added snake locations to map

--- app/map_builder.py
+++ app/map_builder.py
@@ -1,7 +1,7 @@
 SPACE = 0
 WALL = 1
-SNAKE = 2
-FOOD = 3
+FOOD = 2
+SNAKE = 3
 SNAKE_HEAD = 4
 
 
@@ -12,7 +12,16 @@ def build_map(data):
     # fill in food locations
     for food in data['food']['data']:
         map[food['x']][food['y']] = FOOD
+    # fill in snake locations
+    for snake in data['snakes']['data']
+        for segment in snake['body']['data']
+            # get each segment from data{snakes, data, body, data}
+            map[segment['x']][segment['y']] = SNAKE
+        # mark snake head locations
+        map[snake['body']['data'][0]['x']][snake['body']['data'][0]['y']] = SNAKE_HEAD
+
     return map
+
 
 
 def print_map(map, w, h):
