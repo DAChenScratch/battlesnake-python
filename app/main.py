3bb4fa59e7df59705edb364a5db419fd070bb3b5
Tyrel Hiebert <tyrelh@uvic.ca>
2018-02-13 14:17:49 -0800

    Valid move detection working, a* working

--- app/main.py
+++ app/main.py
@@ -12,13 +12,14 @@ ENEMY_HEAD = 4
 WALL = 5
 directions = ['up', 'left', 'down', 'right']
 # general variables
-direction = 0
 game_id = 0
 board_width = 0
 board_height = 0
 
-# a* variables
-
+# my snake variables
+direction = 0
+health = 100
+turn = 0
 
 
 @bottle.route('/static/<path:path>')
@@ -52,18 +53,17 @@ def start():
 # respond on /move
 @bottle.post('/move')
 def move():
-    global direction, directions, board_height, board_width, game_id
+    global direction, directions, board_height, board_width, game_id, health, turn
     data = bottle.request.json
-    # build current map using game data
     start_time = time.time()
-    map = build_map(data)
     # run ai to get next direction
-    direction = kill_time(direction, data)
-    #astar(data)
+    direction = ai(data)
+    health = data['you']['health']
+    turn = data['turn']
     # print data for debugging
     if debug:
-        #print_map(map)
-        print(directions[direction])
+        print('REMAINING HEALTH IS ' + str(health) + ' ON TURN ' + str(turn) + '.')
+        print('SENDING MOVE: ' + str(directions[direction]))
         end_time = time.time()
         print('Time for move was ' + str((end_time - start_time) * 1000) + 'ms')
     # return next move
@@ -73,17 +73,25 @@ def move():
     }
 
 
-# test ai
+# do your thing
+def ai(data):
+    map = build_map(data)
+    close_food = closest_food(data)
+    return astar(data, map, close_food)
+
+
+
+# circle around yourself to kill time safely
 def kill_time(direction, data):
     global board_height, board_width
     if debug:
         print('Killing time..')
         close_food = closest_food(data)
-        grid = build_astar_grid(board_width, board_height)
+        #grid = build_astar_grid(board_width, board_height)
         print('closest food: ' + str(close_food))
-        print('neighbors of food: ')
-        for neighbor in grid[close_food[0]][close_food[1]].neighbors:
-            print(neighbor)
+        #print('neighbors of food: ')
+        #for neighbor in grid[close_food[0]][close_food[1]].neighbors:
+            #print(neighbor)
     direction += 1
     if direction == body_direction(data):
         direction += 1
@@ -143,6 +151,87 @@ def body_direction(data):
         direction = 2
     return direction
 
+# return direction from a to b
+def calculate_direction(a, b, map, data):
+    x = a[0] - b[0]
+    y = a[1] - b[1]
+    direction = 0
+    # directions = ['up', 'left', 'down', 'right']
+    if x < 0:
+        direction = 3
+    elif x > 0:
+        direction = 1
+    elif y < 0:
+        direction = 2
+    count = 0
+    while not valid_move(direction, map, data):
+        if count == 4:
+            print('DEAD END, NO VALID MOVE REMAINING!')
+            print('GAME OVER')
+            break
+        count += 1
+        direction += 1
+        if direction == 4:
+            direction = 0
+    return direction
+
+
+# check if move in direction will kill you
+# return True if valid
+# return False if it will kill you
+def valid_move(d, map, data):
+    global board_height, board_width
+    current = current_location(data)
+    if debug: print('CHECKING IF MOVE IS VALID!')
+    # directions = ['up', 'left', 'down', 'right']
+    # check up direction
+    if d == 0:
+        if current[1] - 1 < 0:
+            if debug: print('up move is OFF THE MAP!')
+            return False
+        if map[current[0]][current[1] - 1] <= 1:
+            if debug: print('up move is valid.')
+            return True
+        else:
+            if debug: print('up move is FATAL!')
+            return False
+    #check left direction
+    if d == 1:
+        if current[0] - 1 < 0:
+            if debug: print('right move is OFF THE MAP!')
+            return False
+        if map[current[0] - 1][current[1]] <= 1:
+            if debug: print('left move is valid.')
+            return True
+        else:
+            if debug: print('left move is FATAL!')
+            return False
+    # check down direction
+    if d == 2:
+        if current[1] + 1 > board_height - 1:
+            if debug: print('down move is OFF THE MAP!')
+            return False
+        if map[current[0]][current[1] + 1] <= 1:
+            if debug: print('down move is valid.')
+            return True
+        else:
+            if debug: print('down move is FATAL!')
+            return False
+    # check right direction
+    if d == 3:
+        if current[0] + 1 > board_width - 1:
+            if debug: print('right move is OFF THE MAP!')
+            return False
+        if map[current[0] + 1][current[1]] <= 1:
+            if debug: print('right move is valid.')
+            return True
+        else:
+            if debug: print('right move is FATAL!')
+            return False
+    # failsafe
+    if d > 3: print('valid_move FAILED! direction IS NOT ONE OF FOUR POSSIBLE MOVES!')
+    return True
+
 
 # return map array
 def build_map(data):
@@ -171,35 +260,76 @@ def print_map(map):
 
 
 # astar search
-# def astar(data, map, destination):
-#     #destination = get_coords(destination)
-#     search_scores = build_astar_grid(data['width'], data['height'])
-#     open_set = set()
-#     closed_set = set()
-#     # set start location to current head location
-#     start = current_location(data)
-#     open_set.add(start)
-#     # while openset is NOT empty keep searching
-#     while open_set:
-#         lowest_cell = [9999, 9999] # x, y
-#         lowest_f = 9999
-#         # find cell with lowest f score
-#         for cell in open_set:
-#             if search_scores[cell[0]][cell[1]].f < lowest_f:
-#                 lowest_f = search_scores[cell[0]][cell[1]].f
-#                 lowest_cell = cell
-#         # found path to destination
-#         if lowest_cell == destination:
-#             something = 0
-#             # TODO figure out what to return, probably a direction
-#             return something
-#         # else continue searching
-#         current_cell = lowest_cell
-#         # update sets
-#         open_set.remove(lowest_cell)
-#         closed_set.add(current_cell)
-#         # check every viable neighbor to current cell
-#         assess_neighbors(current_cell)
+def astar(data, map, destination):
+    global debug
+    if debug: print('destination for search: ' + str(destination))
+    #destination = get_coords(destination)
+    search_scores = build_astar_grid(data['width'], data['height'])
+    open_set = []
+    closed_set = []
+    # set start location to current head location
+    start = current_location(data)
+    if debug: print('Current location for search: ' + str(start))
+    open_set.append(start)
+    #if debug: print('Current location for search in open_set: ' + str(open_set))
+    # while openset is NOT empty keep searching
+    while open_set:
+        #if debug: print('Got here!')
+        lowest_cell = [9999, 9999] # x, y
+        lowest_f = 9999
+        # find cell with lowest f score
+        for cell in open_set:
+            #if debug: print('cell f: ' + str(search_scores[cell[0]][cell[1]].f))
+            if search_scores[cell[0]][cell[1]].f < lowest_f:
+                lowest_f = search_scores[cell[0]][cell[1]].f
+                lowest_cell = cell
+        # found path to destination
+        if lowest_cell[0] == destination[0] and lowest_cell[1] == destination[1]:
+            if debug: print('FOUND A PATH! CALCULATING NEXT MOVE...')
+            # retrace path back to origin to find optimal next move
+            temp = lowest_cell
+            while search_scores[temp[0]][temp[1]].previous[0] != start[0] and search_scores[temp[0]][temp[1]].previous[1] != start[1]:
+                #if debug:
+                    #print('Step back from ' + str(temp))
+                    #print('to ' + str(search_scores[temp[0]][temp[1]].previous))
+                    #print('ending at ' + str(start))
+                temp = search_scores[temp[0]][temp[1]].previous
+            # get direction of next optimal move
+            next_move = calculate_direction(start, temp, map, data)
+            return next_move
+        # else continue searching
+        current = lowest_cell
+        current_cell = search_scores[current[0]][current[1]]
+        # update sets
+        open_set.remove(lowest_cell)
+        closed_set.append(current)
+        # check every viable neighbor to current cell
+        for neighbor in search_scores[current[0]][current[1]].neighbors:
+            neighbor_cell = search_scores[neighbor[0]][neighbor[1]]
+            # check if neighbor has already been evaluated
+            if neighbor not in closed_set:
+                temp_g = current_cell.g + 1
+                shorter = True
+                # check if already evaluated with lower g score
+                if neighbor in open_set:
+                    if temp_g > neighbor_cell.g:
+                        shorter = False
+                # if not in either set, add to open set
+                else:
+                    neighbor
+                    open_set.append(neighbor)
+                # this is the current best path, record it
+                if shorter:
+                    neighbor_cell.g = temp_g
+                    neighbor_cell.h = get_distance(neighbor, destination)
+                    neighbor_cell.f = neighbor_cell.g + neighbor_cell.h
+                    neighbor_cell.previous = current
+                    #if debug: print('added ' + str(current) + ' as previous to ' + str(neighbor))
+    # if reach this point and open set is empty, no path
+    if not open_set:
+        if debug: print('COULD NOT FIND PATH!')
+        return 0
+
 
 
 # return grid of empty Cells for astar search data
