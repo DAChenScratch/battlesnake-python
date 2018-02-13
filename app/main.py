ea8c9da827cd15bdff45e0aa50af774ef7d870c3
Tyrel Hiebert <tyrelh@uvic.ca>
2018-02-13 09:04:40 -0800

    closest_food returning correctly

--- app/main.py
+++ app/main.py
@@ -2,16 +2,19 @@ import bottle
 import os
 import time
 
+debug = True
+# board variables
 SPACE = 0
 FOOD = 1
 MY_HEAD = 2
 SNAKE_BODY = 3
 ENEMY_HEAD = 4
 WALL = 5
-debug = True
-
 directions = ['up', 'left', 'down', 'right']
-test_direction = 0
+# general variables
+direction = 0
+# a* variables
+
 
 
 @bottle.route('/static/<path:path>')
@@ -31,9 +34,7 @@ def start():
         bottle.request.urlparts.scheme,
         bottle.request.urlparts.netloc
     )
-
     # TODO: Do things with data
-
     return {
         'color': '#99ccff',
         'secondary_color': '#99ddff',
@@ -46,32 +47,34 @@ def start():
 # respond on /move
 @bottle.post('/move')
 def move():
-    global test_direction
+    global direction
     global directions
     data = bottle.request.json
-
     # build current map using game data
     start_time = time.time()
     map = build_map(data)
-
     # run ai to get next direction
-    test_direction = test_ai(test_direction, data)
-
+    direction = kill_time(direction, data)
     # print data for debugging
     if debug:
-        print_map(map, data['width'], data['height'])
-        print(directions[test_direction])
+        print_map(map)
+        print(directions[direction])
         end_time = time.time()
         print('Time for move was ' + str((end_time - start_time) * 1000) + 'ms')
-    
+    # return next move
     return {
-        'move': directions[test_direction],
-        'taunt': 'battlesnake-python!'
+        'move': directions[direction],
+        'taunt': 'Not cool.'
     }
 
 
 # test ai
-def test_ai(direction, data):
+def kill_time(direction, data):
+    if debug:
+        print('Killing time..')
+        close_food = closest_food(data)
+        print(close_food)
+
     direction += 1
     if direction == body_direction(data):
         direction += 1
@@ -80,24 +83,55 @@ def test_ai(direction, data):
     return direction
 
 
+# convert object yx to list yx
+def get_coords (o):
+    return (o['x'], o['y'])
+
+
+# return manhattan distance between a and b
+def get_distance(a, b):
+    return (abs(a[0] - b[0]) + abs(a[1] - b[1]))
+
+
+# return coords of closest food to head
+def closest_food(data):
+    current = current_location(data)
+    shortest_distance = -1
+    closest_food = None
+    foods = data['food']['data']
+    # iterate over each piece of food
+    for food in foods:
+        food = get_coords(food)
+        distance = get_distance(current, food)
+        if shortest_distance < 0:
+            shortest_distance = distance
+            closest_food = food
+        else:
+            if distance < shortest_distance:
+                shortest_distance = distance
+                closest_food = food
+    return closest_food
+
+
+# return x,y coords of current head location
+def current_location(data):
+    return (data['you']['body']['data'][0]['x'], data['you']['body']['data'][0]['y'])
+
+
 # return direction of previous body segment from head segment
 def body_direction(data):
-    x1 = data['you']['body']['data'][0]['x']
-    y1 = data['you']['body']['data'][0]['y']
-    x2 = data['you']['body']['data'][1]['x']
-    y2 = data['you']['body']['data'][1]['y']
-    x = x1 - x2
-    y = y1 - y2
-    direction = None
-    #directions = ['up', 'left', 'down', 'right']
+    coord_head = get_coords(data['you']['body']['data'][0])
+    coord_body = get_coords(data['you']['body']['data'][1])
+    x = coord_head[0] - coord_body[0]
+    y = coord_head[1] - coord_body[1]
+    direction = 0
+    # directions = ['up', 'left', 'down', 'right']
     if x < 0:
         direction = 3
     elif x > 0:
         direction = 1
     elif y < 0:
         direction = 2
-    elif y > 0:
-        direction = 0
     return direction
 
 
@@ -122,10 +156,9 @@ def build_map(data):
 
 
 # print whole map
-def print_map(map, w, h):
+def print_map(map):
     for row in map:
         print(str(row))
-        #print('\n')
 
 
 # Expose WSGI app (so gunicorn can find it)
